2bbbab3405941581b5a91eb7f2706c9fc5aa2de4
PeZoi <pezoiks1@gmail.com>
2023-12-13 22:18:13 +0700

    page permission

--- car-management/form-car.php
+++ car-management/form-car.php
@@ -4,7 +4,7 @@
 <?php
 session_start();
 # Kiểm tra xem có phải là role admin không, nếu không thì không cho vào trang này
-if ((isset($_SESSION['role']) && $_SESSION['role'] !== 'admin')) {
+if ((!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin')) {
    header("Location: ../index.php");
 }
 
--- car-management/handle-delete-car.php
+++ car-management/handle-delete-car.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+# Kiểm tra xem có phải là role admin không, nếu không thì không cho vào trang này
+if ((!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin')) {
+   header("Location: ../index.php");
+}
+
 include "../database/db_connect.php";
 $db = new DB_Connect();
 if (isset($_GET['id'])) {
